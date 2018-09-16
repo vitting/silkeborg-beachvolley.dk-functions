c7a2802d24ba26e3258e3bac0c14b1b4a9e09ed7
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-09-16 21:54:41 +0200

    added update of player on new match and set node version to 8

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -70,3 +70,97 @@ export const deleteBulletinPlayersCommittedCount = functions.firestore.document(
         return bulletinsDocRef.update({"numberOfPlayersCommitted": count});
     });
 });
+
+export const onNewsMatchUpdatePointsAndPlayers = functions.firestore.document("/ranking_matches/{documentId}").onCreate(async (snap, context) => {
+    const winnerPoints:number = 10;
+    const loserPoints:number = 5;
+    const data = snap.data();
+    const matchDocumentId = context.params.documentId;
+    const winner1Id: string = data.winner1.id;
+    const winner2Id: string = data.winner2.id;
+    const loser1Id: string = data.loser1.id;
+    const loser2Id: string = data.loser2.id;
+    const playerWinner1Ref = admin.firestore().collection("ranking_players").doc(winner1Id);
+    const playerWinner2Ref = admin.firestore().collection("ranking_players").doc(winner2Id);
+    const playerLoser1Ref = admin.firestore().collection("ranking_players").doc(loser1Id);
+    const playerLoser2Ref = admin.firestore().collection("ranking_players").doc(loser2Id);
+    const playerLoser1Doc = await playerLoser1Ref.get();
+    const playerLoser2Doc = await playerLoser2Ref.get();
+    const playerWinner1Doc = await playerWinner1Ref.get();
+    const playerWinner2Doc = await playerWinner2Ref.get();
+    const matchRef = admin.firestore().collection("ranking_matches").doc(matchDocumentId);
+    let playerWinner1PointTotal = <number>playerWinner1Doc.data().points.total;
+    let playerWinner1PointWon = <number>playerWinner1Doc.data().points.won;
+    let playerWinner2PointTotal = <number>playerWinner2Doc.data().points.total;
+    let playerWinner2PointWon = <number>playerWinner2Doc.data().points.won;
+    let playerLoser1PointTotal = <number>playerLoser1Doc.data().points.total;
+    let playerLoser1PointLost = <number>playerLoser1Doc.data().points.lost;
+    let playerLoser2PointTotal = <number>playerLoser2Doc.data().points.total;
+    let playerLoser2PointLost = <number>playerLoser2Doc.data().points.lost;
+    let playerWinner1NumberOfMatchesWon = <number>playerWinner1Doc.data().numberOfPlayedMatches.won;
+    let playerWinner1NumberOfMatchesTotal = <number>playerWinner1Doc.data().numberOfPlayedMatches.total;
+    let playerWinner2NumberOfMatchesWon = <number>playerWinner2Doc.data().numberOfPlayedMatches.won;
+    let playerWinner2NumberOfMatchesTotal = <number>playerWinner2Doc.data().numberOfPlayedMatches.total;
+    let playerLoser1NumberOfMatchesLost = <number>playerLoser1Doc.data().numberOfPlayedMatches.lost;
+    let playerLoser1NumberOfMatchesTotal = <number>playerLoser1Doc.data().numberOfPlayedMatches.total;
+    let playerLoser2NumberOfMatchesLost = <number>playerLoser2Doc.data().numberOfPlayedMatches.lost;
+    let playerLoser2NumberOfMatchesTotal = <number>playerLoser2Doc.data().numberOfPlayedMatches.total;
+    
+    playerWinner1PointTotal = playerWinner1PointTotal + winnerPoints;
+    playerWinner1PointWon = playerWinner1PointWon + winnerPoints;
+    playerWinner2PointTotal = playerWinner2PointTotal + winnerPoints;
+    playerWinner2PointWon = playerWinner2PointWon + winnerPoints;
+    playerLoser1PointTotal = playerLoser1PointTotal + loserPoints;
+    playerLoser1PointLost = playerLoser1PointLost + loserPoints;
+    playerLoser2PointTotal = playerLoser2PointTotal + loserPoints;
+    playerLoser2PointLost = playerLoser2PointLost + loserPoints;
+
+    playerWinner1NumberOfMatchesTotal++;
+    playerWinner1NumberOfMatchesWon++;
+    playerWinner2NumberOfMatchesTotal++;
+    playerWinner2NumberOfMatchesWon++;
+    playerLoser1NumberOfMatchesTotal++; 
+    playerLoser1NumberOfMatchesLost++;
+    playerLoser2NumberOfMatchesTotal++; 
+    playerLoser2NumberOfMatchesLost++;
+
+    await matchRef.update({
+        "winner1.points": winnerPoints,
+        "winner2.points": winnerPoints,
+        "loser1.points": loserPoints,
+        "loser2.points": loserPoints
+    });
+
+    await playerWinner1Ref.update({
+        "numberOfPlayedMatches.total": playerWinner1NumberOfMatchesTotal,
+        "numberOfPlayedMatches.won": playerWinner1NumberOfMatchesWon,
+        "points.total": playerWinner1PointTotal,
+        "points.won": playerWinner1PointWon        
+    });
+
+    await playerWinner2Ref.update({
+        "numberOfPlayedMatches.total": playerWinner2NumberOfMatchesTotal,
+        "numberOfPlayedMatches.won": playerWinner2NumberOfMatchesWon,
+        "points.total": playerWinner2PointTotal,
+        "points.won": playerWinner2PointWon        
+    });
+
+    await playerLoser1Ref.update({
+        "numberOfPlayedMatches.total": playerLoser1NumberOfMatchesTotal,
+        "numberOfPlayedMatches.lost": playerLoser1NumberOfMatchesLost,
+        "points.total": playerLoser1PointTotal,
+        "points.lost": playerLoser1PointLost        
+    });
+
+    await playerLoser2Ref.update({
+        "numberOfPlayedMatches.total": playerLoser2NumberOfMatchesTotal,
+        "numberOfPlayedMatches.lost": playerLoser2NumberOfMatchesLost,
+        "points.total": playerLoser2PointTotal,
+        "points.lost": playerLoser2PointLost        
+    });
+    //Some calculation
+    //Update players WonLostDta
+    //Update data and update it in ranking_matches
+
+    return null;
+});
