ccb6c0cea78f8a39ca596f2d45a8cc19ce6ec522
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-10-15 12:54:35 +0200

    added onDelete of ranking match

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,7 +15,7 @@ export const notificationOnAddBulletin = functions.firestore.document("/bulletin
     const authorName: string = data.author.name;
     const authorId: string = data.author.id;
     const creationDate: Date = data.creationDate.toDate();
-
+    
     if (bulletinId !== "" && bulletinType !== "") {
         const snapshot = await admin.firestore().collection("users_messaging").where("subscriptions", "array-contains", bulletinType).get();
         if (!snapshot.empty) {
@@ -30,6 +30,7 @@ export const notificationOnAddBulletin = functions.firestore.document("/bulletin
             if (listOfDevices.length > 0) {
                 const notificationTitle: string = authorName + " har oprettet et nyt opslag";
                 const notificationBody: string = "";
+
                 const payload: admin.messaging.MessagingPayload = {
                     notification: {
                         title: notificationTitle,
@@ -37,7 +38,7 @@ export const notificationOnAddBulletin = functions.firestore.document("/bulletin
                     },
                     data: {
                         "click_action": "FLUTTER_NOTIFICATION_CLICK",
-                        "type": "bulletin",
+                        "dataType": "bulletin",
                         "bulletinType": bulletinType
                     }
                 };
@@ -121,6 +122,77 @@ export const deleteBulletinCommittedCount = functions.firestore.document("/bulle
 
         return bulletinsDocRef.update({"numberOfCommits": count});
     });
+});
+
+export const onMatchDeleteUpdatePlayers = functions.firestore.document("/ranking_matches/{documentId}").onDelete(async (snap, context) => {
+    const match = snap.data();
+    const matchWinner1Id: string = match.winner1.id;
+    const matchWinner2Id: string = match.winner2.id;
+    const matchLoser1Id: string = match.loser1.id;
+    const matchLoser2Id: string = match.loser2.id;
+    const matchWinner1Points: number = <number>match.winner1.points;
+    const matchWinner2Points: number = <number>match.winner2.points;
+    const matchLoser1Points: number = <number>match.loser1.points;
+    const matchLoser2Points: number = <number>match.loser2.points;
+    const playerWinner1Ref = admin.firestore().collection("ranking_players").doc(matchWinner1Id);
+    const playerWinner2Ref = admin.firestore().collection("ranking_players").doc(matchWinner2Id);
+    const playerLoser1Ref = admin.firestore().collection("ranking_players").doc(matchLoser1Id);
+    const playerLoser2Ref = admin.firestore().collection("ranking_players").doc(matchLoser2Id);
+    const playerWinner1Doc = await playerWinner1Ref.get();
+    const playerWinner2Doc = await playerWinner2Ref.get();
+    const playerLoser1Doc = await playerLoser1Ref.get();
+    const playerLoser2Doc = await playerLoser2Ref.get();
+
+    //Winner1
+    const playerWinner1PointsTotal: number = <number>playerWinner1Doc.data().points.total;
+    const playerWinner1PointsWon: number = <number>playerWinner1Doc.data().points.won;
+    const playerWinner1NumberOfMatchesTotal: number = <number>playerWinner1Doc.data().numberOfPlayedMatches.total;
+    const playerWinner1NumberOfMatchesWon: number = <number>playerWinner1Doc.data().numberOfPlayedMatches.won;
+    //Winner2
+    const playerWinner2PointsTotal: number = <number>playerWinner2Doc.data().points.total;
+    const playerWinner2PointsWon: number = <number>playerWinner2Doc.data().points.won;
+    const playerWinner2NumberOfMatchesTotal: number = <number>playerWinner2Doc.data().numberOfPlayedMatches.total;
+    const playerWinner2NumberOfMatchesWon: number = <number>playerWinner2Doc.data().numberOfPlayedMatches.won;
+    //Loser1
+    const playerLoser1PointsTotal: number = <number>playerLoser1Doc.data().points.total;
+    const playerLoser1PointsLost: number = <number>playerLoser1Doc.data().points.lost;
+    const playerLoser1NumberOfMatchesTotal: number = <number>playerLoser1Doc.data().numberOfPlayedMatches.total;
+    const playerLoser1NumberOfMatchesLost: number = <number>playerLoser1Doc.data().numberOfPlayedMatches.lost;
+    //Loser2
+    const playerLoser2PointsTotal: number = <number>playerLoser2Doc.data().points.total;
+    const playerLoser2PointsLost: number = <number>playerLoser2Doc.data().points.lost;
+    const playerLoser2NumberOfMatchesTotal: number = <number>playerLoser2Doc.data().numberOfPlayedMatches.total;
+    const playerLoser2NumberOfMatchesLost: number = <number>playerLoser2Doc.data().numberOfPlayedMatches.lost;
+
+    await playerWinner1Ref.update({
+        "points.won": playerWinner1PointsWon - matchWinner1Points,
+        "points.total": playerWinner1PointsTotal - matchWinner1Points,
+        "numberOfPlayedMatches.won": playerWinner1NumberOfMatchesWon - 1,
+        "numberOfPlayedMatches.total": playerWinner1NumberOfMatchesTotal - 1
+    });
+
+    await playerWinner2Ref.update({
+        "points.won": playerWinner2PointsWon - matchWinner2Points,
+        "points.total": playerWinner2PointsTotal - matchWinner2Points,
+        "numberOfPlayedMatches.won": playerWinner2NumberOfMatchesWon - 1,
+        "numberOfPlayedMatches.total": playerWinner2NumberOfMatchesTotal - 1
+    });
+
+    await playerLoser1Ref.update({
+        "points.lost": playerLoser1PointsLost - matchLoser1Points,
+        "points.total": playerLoser1PointsTotal - matchLoser1Points,
+        "numberOfPlayedMatches.lost": playerLoser1NumberOfMatchesLost - 1,
+        "numberOfPlayedMatches.total": playerLoser1NumberOfMatchesTotal - 1
+    });
+
+    await playerLoser2Ref.update({
+        "points.lost": playerLoser2PointsLost - matchLoser2Points,
+        "points.total": playerLoser2PointsTotal - matchLoser2Points,
+        "numberOfPlayedMatches.lost": playerLoser2NumberOfMatchesLost - 1,
+        "numberOfPlayedMatches.total": playerLoser2NumberOfMatchesTotal - 1
+    });
+
+    return null;
 });
 
 export const onNewsMatchUpdatePointsAndPlayers = functions.firestore.document("/ranking_matches/{documentId}").onCreate(async (snap, context) => {
