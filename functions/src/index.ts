1fb561a923a4ff712594c3aac944dc45c15785ab
Christian Nicolaisen <cvn_vitting@hotmail.com>
2018-10-18 23:14:40 +0200

    added function reset ranking

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 import { isNumber } from 'util';
-import * as moment from 'moment';
 
 admin.initializeApp();
 
@@ -379,3 +378,60 @@ export const getBulletinsCount = functions.https.onCall(async (data, context) =>
         playCount: playCount
     };
 });
+
+export const resetRanking = functions.https.onCall(async (data, context) => {
+    try {
+        const collectionMatchesName: string = "ranking_matches";
+        const collectionPlayersName: string = "ranking_players";
+        const collectionPlayersArchiveName: string = "ranking_players_archive";
+        const matchesQuerySnapshot = await admin.firestore().collection(collectionMatchesName).where("enabled", "==", true).get();
+        const playersQuerySnapshot = await admin.firestore().collection(collectionPlayersName).get();
+        const playersArchiveRef = await admin.firestore().collection(collectionPlayersArchiveName);
+        const promiseListMatches = [];
+        const promiseListPlayersArchive = [];
+        const promiseListPlayers = [];
+
+        await saveToLog(context.auth.uid, "resetRanking");
+
+        matchesQuerySnapshot.docs.forEach(async (snapshot, index) => {
+            promiseListMatches.push(snapshot.ref.update({ "enabled": false }));
+        });
+
+        await Promise.all(promiseListMatches);
+
+        playersQuerySnapshot.docs.forEach(async (player, index) => {
+            promiseListPlayersArchive.push(playersArchiveRef.add(player.data()));
+            promiseListPlayers.push(player.ref.update({
+                "numberOfPlayedMatches.total": 0,
+                "numberOfPlayedMatches.won": 0,
+                "numberOfPlayedMatches.lost": 0,
+                "points.total": 0,
+                "points.won": 0,
+                "points.lost": 0
+            }));
+        });
+
+        await Promise.all(promiseListPlayersArchive);
+        await Promise.all(promiseListPlayers);
+
+        return {
+            "result": "ok"
+        };
+    } catch (error) {
+        console.log("ERROR resetRanking", error);
+
+        return {
+            "result": "error",
+            "error": error
+        };
+    }
+});
+
+function saveToLog(userId: string, command: string) {
+    const admin_log_collection = "admin_logs";
+    return admin.firestore().collection(admin_log_collection).add({
+        "userId": userId,
+        "date": admin.firestore.FieldValue.serverTimestamp(),
+        "command": command
+    });
+}
